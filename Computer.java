6a21f0cdae43e2ce8d3771abbb4b305b7e5ce753
CEC <learnmorececc1@gmail.com>
2025-07-17 11:44:31 +0200

    Kommentare hinzugefügt.

--- Computer.java
+++ Computer.java
@@ -1,9 +1,14 @@
 public class Computer
 {
-    private String hersteller;
-    private boolean laptop;
-    private int preis;
+    private String hersteller; // Eigenschaft der Name des Herstellers
+    private boolean laptop;     // Ob es Laptops gibt oder nicht (Ja oder Nein)
+    private int preis;          // Wie viel es kostet
     
+    
+    /*
+     * It is creating a new Instance of the class Computer.
+     * 
+     */
     private Computer(String newHersteller, boolean newLaptop, int newPreis)
     {
      setHersteller(newHersteller);
@@ -13,16 +18,19 @@ public class Computer
     
     public void setHersteller(String newHersteller)
     {
+        // set the property hersteller to the value of the parameter newHersteller.
         hersteller = newHersteller;
     }
     
     public void setLaptop(boolean newLaptop)
     {
+        // set the property laptop to the value of the parameter newLaptop.
         laptop = newLaptop;
     }
     
     public void setPreis(int newPreis)
     {
+        // set the property preis to the value of the newPreis.
         preis = newPreis;
     }
     
