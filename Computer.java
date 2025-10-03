3d5db74fc37bd2a973d4099e7f22e58ee961ae33
CEC <learnmorececc1@gmail.com>
2025-10-03 23:36:27 +0200

    printComputer Format geändert mit if Abfrage

--- Computer.java
+++ Computer.java
@@ -3,24 +3,47 @@ public class Computer
     private String hersteller; // Eigenschaft; Der Name des Herstellers
     private boolean laptop;     // Ob es Laptops gibt oder nicht (Ja oder Nein)
     private int preis;          // Wie viel es kostet
+    private int mhz;
     
     
     /*
      * It is creating a new Instance of the class Computer.
      * 
      */
-    public Computer(String hersteller, boolean laptop, int preis)
+    public Computer(String hersteller, boolean laptop, int preis,int mhz)
     {
      setHersteller(hersteller);
      setLaptop(laptop);
      setPreis(preis);
+     setMhz(mhz);
     }
-    
+    public Computer(String hersteller, boolean laptop, int preis)
+    {
+        setHersteller(hersteller);
+        setLaptop(laptop);
+        setPreis(preis);
+        setMhz(2000);
+    }
+    public Computer(String hersteller, boolean laptop)
+    {
+        setHersteller(hersteller);
+        setLaptop(laptop);
+        setPreis(899);
+        setMhz(2000);
+    }
+    public Computer(String hersteller, int preis)
+    {
+        setHersteller(hersteller);
+        setLaptop(true);
+        setPreis(899);
+        setMhz(2000);
+    }
     public Computer()
     {
-     setHersteller("Intel");
+     setHersteller("Lenovo");
      setLaptop(true);
-     setPreis(2000);
+     setPreis(899);
+     setMhz(2000);
     }
     
     public void setHersteller(String hersteller)
@@ -28,32 +51,46 @@ public class Computer
         // set the property hersteller to the value of the parameter newHersteller.
         this.hersteller = hersteller;
     }
-        public void setLaptop(boolean laptop)
+    public void setLaptop(boolean laptop)
     {
         // set the property laptop to the value of the parameter newLaptop.
         this.laptop = laptop;
     }
-       public void setPreis(int preis)
+    public void setPreis(int preis)
     {
         // set the property preis to the value of the newPreis.
         this.preis = preis;
+    }
+    public void setMhz(int mhz)
+    {
+        this.mhz = mhz;
     }
     
     public String getHersteller()
     {
         return hersteller;
     }
-        public boolean getLaptop()
+    public boolean getLaptop()
     {
         return laptop;
     }
-        public int getPreis()
+      public int getPreis()
     {
         return preis;
+    }
+    public int getMhz()
+    {
+        return mhz;
     }
     
    public void printComputer()
     {
-    System.out.println("Hersteller: " + hersteller + ", Laptop: " + laptop + ", Preis: " + preis + " Euro");
+    if(laptop == true)
+    {
+    System.out.println("Hersteller: " + hersteller + ", Laptop ist vorhanden, Preis: " + preis + " Euro");
+    }
+    else{
+    System.out.println("Hersteller: " + hersteller + ", Computer ist vorhanden, Preis: " + preis + " Euro"); 
+    }
     }
    }
